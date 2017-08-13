d3ef288ee4c136e0204133f7136032b1cf6a851a
Surendra <no-reply@testingdocs.com>
2017-08-13 23:46:01 +0530

    Adding sample web project

---
+++ MyWebProject/src/com/testingdocs/servlet/examples/HelloWorldExample.java
@@ -0,0 +1,56 @@
+package com.testingdocs.servlet.examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldExample
+ */
+@WebServlet(description = "Sample TestingDocs Servlet Example", urlPatterns = { "/HelloWorldExample" })
+public class HelloWorldExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldExample() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		{
+	        response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+	        out.println("<html>");
+	        out.println("<head>");
+	        out.println("<title>Hello World!</title>");
+	        out.println("</head>");
+	        out.println("<body>");
+	        out.println("<h1>Hello World!</h1>");
+	        out.println("Visit www.TestingDocs.com for interesting QA stuff..!!");
+	        out.println("</body>");
+	        out.println("</html>");
+	    }
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
